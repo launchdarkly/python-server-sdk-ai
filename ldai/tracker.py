fe50860adb89173d08efd298db0eea9818f7a8f3
Daniel OBrien <dobrien@launchdarkly.com>
2024-10-18 08:57:19 -0400

    add tracking decorator

--- ldai/tracker.py
+++ ldai/tracker.py
@@ -1,3 +1,4 @@
+import time
 from typing import Dict, Union
 from ldclient import Context, LDClient
 from ldai.types import BedrockTokenUsage, FeedbackKind, TokenUsage, UnderscoreTokenUsage
@@ -17,6 +18,16 @@ class LDAIConfigTracker:
     
     def track_duration(self, duration: int) -> None:
         self.ld_client.track('$ld:ai:duration:total', self.context, self.get_track_data(), duration)
+
+    def track_duration_of(self, func):
+            def wrapper(*args, **kwargs):
+                start_time = time.time()
+                result = func(*args, **kwargs)
+                end_time = time.time()
+                duration = int((end_time - start_time) * 1000)  # duration in milliseconds
+                self.track_duration(duration)
+                return result
+            return wrapper
 
     def track_tokens(self, tokens: Union[TokenUsage, UnderscoreTokenUsage, BedrockTokenUsage]) -> None:
         token_metrics = tokens.to_metrics()
