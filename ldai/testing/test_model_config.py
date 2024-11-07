6d5d8fb58cb1eb7babbf5da776bae2bc77657827
Daniel OBrien <dobrien@launchdarkly.com>
2024-11-07 14:16:02 -0500

    fix: More fixture changes

--- ldai/testing/test_model_config.py
+++ ldai/testing/test_model_config.py
@@ -41,16 +41,16 @@ def client(td: TestData) -> LDClient:
     return LDClient(config=config)
 
 @pytest.fixture
-def tracker(td: TestData) -> LDAIConfigTracker:
-    return LDAIConfigTracker(client(td), 'abcd', 'model-config', Context.create('user-key'))
+def tracker() -> LDAIConfigTracker:
+    return LDAIConfigTracker(client, 'abcd', 'model-config', Context.create('user-key'))
 
 @pytest.fixture
 def ldai_client(client: LDClient) -> LDAIClient:
     return LDAIClient(client)
 
-def test_model_config_interpolation(ldai_client: LDAIClient, td):
+def test_model_config_interpolation(ldai_client: LDAIClient, tracker):
     context = Context.create('user-key')
-    default_value = AIConfig(config=AIConfigData(model={ 'modelId': 'fakeModel'}, prompt=[LDMessage(role='system', content='Hello, {{name}}!')]), tracker=tracker(td), enabled=True)
+    default_value = AIConfig(config=AIConfigData(model={ 'modelId': 'fakeModel'}, prompt=[LDMessage(role='system', content='Hello, {{name}}!')]), tracker=tracker, enabled=True)
     variables = {'name': 'World'}
 
     config = ldai_client.model_config('model-config', context, default_value, variables)
@@ -60,9 +60,9 @@ def test_model_config_interpolation(ldai_client: LDAIClient, td):
     assert config.config.prompt[0].content == 'Hello, World!'
     assert config.enabled is True
 
-def test_model_config_no_variables(ldai_client: LDAIClient, td):
+def test_model_config_no_variables(ldai_client: LDAIClient, tracker):
     context = Context.create('user-key')
-    default_value = AIConfig(config=AIConfigData(model={}, prompt=[]), tracker=tracker(td), enabled=True)
+    default_value = AIConfig(config=AIConfigData(model={}, prompt=[]), tracker=tracker, enabled=True)
 
     config = ldai_client.model_config('model-config', context, default_value, {})
 
@@ -71,9 +71,9 @@ def test_model_config_no_variables(ldai_client: LDAIClient, td):
     assert config.config.prompt[0].content == 'Hello, !'
     assert config.enabled is True
 
-def test_context_interpolation(ldai_client: LDAIClient, td):
+def test_context_interpolation(ldai_client: LDAIClient, tracker):
     context = Context.builder('user-key').name("Sandy").build()
-    default_value = AIConfig(config=AIConfigData(model={}, prompt=[]), tracker=tracker(td), enabled=True)
+    default_value = AIConfig(config=AIConfigData(model={}, prompt=[]), tracker=tracker, enabled=True)
     variables = {'name': 'World'}
 
     config = ldai_client.model_config('ctx-interpolation', context, default_value, variables)
@@ -83,9 +83,9 @@ def test_context_interpolation(ldai_client: LDAIClient, td):
     assert config.config.prompt[0].content == 'Hello, Sandy!'
     assert config.enabled is True
 
-def test_model_config_multiple(ldai_client: LDAIClient, td):
+def test_model_config_multiple(ldai_client: LDAIClient, tracker):
     context = Context.create('user-key')
-    default_value = AIConfig(config=AIConfigData(model={}, prompt=[]), tracker=tracker(td), enabled=True)
+    default_value = AIConfig(config=AIConfigData(model={}, prompt=[]), tracker=tracker, enabled=True)
     variables = {'name': 'World', 'day': 'Monday'}
 
     config = ldai_client.model_config('multiple-prompt', context, default_value, variables)
@@ -96,9 +96,9 @@ def test_model_config_multiple(ldai_client: LDAIClient, td):
     assert config.config.prompt[1].content == 'The day is, Monday!'
     assert config.enabled is True
 
-def test_model_config_disabled(ldai_client: LDAIClient, td):
+def test_model_config_disabled(ldai_client: LDAIClient, tracker):
     context = Context.create('user-key')
-    default_value = AIConfig(config=AIConfigData(model={}, prompt=[]), tracker=tracker(td), enabled=False)
+    default_value = AIConfig(config=AIConfigData(model={}, prompt=[]), tracker=tracker, enabled=False)
 
     config = ldai_client.model_config('off-config', context, default_value, {})
 
