b94c9f7d588738d91be0f0f29961e7860c8ae405
Edwin Okonkwo <edwintop15@gmail.com>
2025-11-19 09:41:49 +0100

    Eiminate unused components.

--- ldai/chat/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Chat module for LaunchDarkly AI SDK."""
-
-from ldai.chat.tracked_chat import TrackedChat
-
-__all__ = ['TrackedChat']
-
--- ldai/judge/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Judge module for LaunchDarkly AI SDK."""
-
-from ldai.judge.ai_judge import AIJudge
-
-__all__ = ['AIJudge']
-
-
